5abd8a42883deb729c4eb12daca2cbc29b359510
Tareq <tareqaldhaifani@gmail.com>
2023-07-20 23:23:03 +0300

    task 3

---
+++ python-functions/3-string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def reverse_string(string):
+    length = len(string)
+    rev_string = ""
+    for i in range(length - 1, -1, -1):
+        rev_string += string[i]
+    return (rev_string)
