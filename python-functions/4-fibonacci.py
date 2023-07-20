c7364e78eae8144360672d2ed33d283320c4cc18
Tareq <tareqaldhaifani@gmail.com>
2023-07-20 23:32:47 +0300

    task 4

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fibonacci_sequence(n):
+    if n <= 0:
+        return([])
+    elif n == 1:
+        return([0])
+    elif n == 2:
+        return([0, 1])
+    else:
+        fib_sequence = [0, 1]
+        for i in range(1, n - 1):
+            fib_sequence.append(fib_sequence[i] + fib_sequence[i - 1])
+        return(fib_sequence)
