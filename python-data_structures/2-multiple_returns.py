b04160b7b5ab4c27cb99fa6b0a1337b3cb9729b3
Tareq <tareqaldhaifani@gmail.com>
2023-07-24 17:12:38 +0300

    task 2 - updated to account for empty strings

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    t = (len(sentence), sentence[0])
+    length = len(sentence)
+    a = sentence[0] if length > 0 else None
+    t = (len(sentence), a)
     return t
