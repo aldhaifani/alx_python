71db2464670bf4596135ad1f329881124818a16b
Tareq <tareqaldhaifani@gmail.com>
2023-07-24 17:00:45 +0300

    task 1 - adjusted indentations to align with the PEP8 validation

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -3,10 +3,10 @@ def print_matrix_integer(matrix=[[]]):
     row_length = len(matrix[0])
     if row_length > 0:
         for row in matrix:
-              for i in range(row_length):
-                  if i == row_length - 1:
-                      print("{:d}".format(row[i]))
-                  else:
-                       print("{:d}".format(row[i]), end=" ")
+            for i in range(row_length):
+                if i == row_length - 1:
+                    print("{:d}".format(row[i]))
+                else:
+                    print("{:d}".format(row[i]), end=" ")
     else:
-         print()
+        print()
