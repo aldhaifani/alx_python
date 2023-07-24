e24de0fef8369fa2ddbb6a2bb1cd3f50942ffd16
Tareq <tareqaldhaifani@gmail.com>
2023-07-24 16:51:17 +0300

    task 1

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    row_length = len(matrix[0])
+    if row_length > 0:
+        for row in matrix:
+              for i in range(row_length):
+                  if i == row_length - 1:
+                      print(row[i])
+                  else:
+                       print("{}".format(row[i]), end=" ")
+    else:
+         print()
