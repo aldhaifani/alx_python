5d5a11c66619741dbb4328d0dd52255195ae066d
Tareq <tareqaldhaifani@gmail.com>
2023-08-08 20:17:54 +0400

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module that defines the class Square"""
+
+
+class Square:
+    """
+    A class that defines a square
+    """
+
+    __size = 0
+
+    def __init__(self, size=0) -> None:
+        """Instantiation function"""
+        if type(size) is not int:
+            raise (TypeError("size must be an integer"))
+        elif size < 0:
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = size
