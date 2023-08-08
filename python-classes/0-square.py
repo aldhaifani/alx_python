6b6879fd9d8b54f5672ea6a0dfe41f2ee6b36cb7
Tareq <tareqaldhaifani@gmail.com>
2023-08-08 20:11:32 +0400

    task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,14 @@
+#!/usr/bin/python3
+"""Module that defines the class Square"""
+
+
 class Square:
+    """
+    A class that defines a square
+    """
+
     __size = 0
 
     def __init__(self, size) -> None:
+        """Instantiation function"""
         self.__size = size
