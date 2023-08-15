89036aa7b58fb6005075909435403a8036c04f01
Tareq <tareqaldhaifani@gmail.com>
2023-08-15 10:19:57 +0400

    task 8

---
+++ python-inheritance/8-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A module that contains the class Sqaure which inherits from Rectangle
+"""
+
+
+Rectangle = __import__("7-rectangle").Rectangle
+
+
+class Squar(Rectangle):
+    """ "
+    A class that defines a Square that inherits from Rectangle
+    """
+
+    def __init__(self, size):
+        """Instantiation method"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
