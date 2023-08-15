412dc47ac50528f4323a56804ef4951fe26fa8b2
Tareq <tareqaldhaifani@gmail.com>
2023-08-15 10:44:32 +0400

    task 0

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+A module that contains the Base class
+"""
+
+
+class Base:
+    """The Base class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Instantiation method"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
