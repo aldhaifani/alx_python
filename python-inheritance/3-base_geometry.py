9a23ac4ed36a2f4f2957546789676af06d39d06a
Tareq <tareqaldhaifani@gmail.com>
2023-08-15 05:54:17 +0400

    task 3 - used the magic method __dir__ to overrid the output of dir()

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -6,4 +6,7 @@ A module that contains an empty class
 
 class BaseGeometry:
     "An empty class"
-    pass
+
+    def __dir__(self):
+        """Overrids the output of dir()"""
+        return [attr for attr in super().__dir__() if attr != "__init_subclass__"]
