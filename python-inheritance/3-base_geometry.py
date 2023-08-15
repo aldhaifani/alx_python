0b16d9d3dd2b3be91b92d8cdbee3cadcf212f453
Tareq <tareqaldhaifani@gmail.com>
2023-08-15 06:24:33 +0400

    task 3 - created a metaclass for the BaseGeometry class

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -4,9 +4,19 @@ A module that contains an empty class
 """
 
 
-class BaseGeometry:
+class BaseGeometryMetaClass(type):
+    """
+    The meta class of BaseGeometry
+    """
+
+    def __dir__(cls) -> None:
+        """Overrids the output of dir()"""
+        return [attr for attr in super().__dir__() if attr != "__init_subclass__"]
+
+
+class BaseGeometry(metaclass=BaseGeometryMetaClass):
     "An empty class"
 
-    def __dir__(self):
+    def __dir__(cls) -> None:
         """Overrids the output of dir()"""
         return [attr for attr in super().__dir__() if attr != "__init_subclass__"]
