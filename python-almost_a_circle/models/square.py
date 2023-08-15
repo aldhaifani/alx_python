9801c3a5c9d2a226842cbedf3d006d7267dd34e0
Tareq <tareqaldhaifani@gmail.com>
2023-08-15 12:25:24 +0400

    task 10

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -22,3 +22,18 @@ class Square(Rectangle):
         return "[Square] ({:d}) {:d}/{:d} - {:d}".format(
             self.id, self.x, self.y, self.width
         )
+
+    @property
+    def size(self):
+        """Public getter method for size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Public setter method for size"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
