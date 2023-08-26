848c8da04240e495081c90bfc5f1f476fa807159
Tareq <tareqaldhaifani@gmail.com>
2023-08-26 06:47:23 +0400

    task 0 - updated the tab size

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -7,5 +7,5 @@ import requests
 r = requests.get("https://alu-intranet.hbtn.io/status")
 
 print("Body response:")
-print("    - type: {}".format(type(r.text)))
-print("    - content: {}".format(r.text))
+print("	- type: {}".format(type(r.text)))
+print("	- content: {}".format(r.text))
