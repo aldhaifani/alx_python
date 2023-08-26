ead11ec502284f16d7d0c379068ecc12425616fc
Tareq <tareqaldhaifani@gmail.com>
2023-08-26 13:13:53 +0400

    task 5

---
+++ python-network_1/6-my_github.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+a Python script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+"""
+
+import requests
+import sys
+
+
+username = sys.argv[1]
+passwd = sys.argv[2]
+
+
+r = requests.get(
+    "https://api.github.com/users/{}".format(username), data={"Authorization": passwd}
+)
+
+user_id = r.text.split(",")[1].split(":")[1]
+print(user_id)
