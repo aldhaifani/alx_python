6b956914b0faaa709825f6d989242ce89de69e51
Tareq <tareqaldhaifani@gmail.com>
2023-08-26 11:42:44 +0400

    task 4

---
+++ python-network_1/5-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a letter
+and sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+import requests
+import sys
+
+
+try:
+    q = sys.argv[1]
+except IndexError:
+    q = ""
+
+r = requests.post("http://0.0.0.0:5000/search_user", json={"q": q})
+
+try:
+    body = eval(r.text)
+    print("[{}] {}".format(body[id], body[name]))
+except (SyntaxError, NameError):
+    print("No result")
