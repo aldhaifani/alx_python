1e07c7837bff9745ec217432ceb8a257b4d99063
Tareq <tareqaldhaifani@gmail.com>
2023-08-26 11:23:32 +0400

    task 3

---
+++ python-network_1/4-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a URL,
+sends a request to the URL and displays the body of the response.
+"""
+import requests
+import sys
+
+url = sys.argv[1]
+
+r = requests.post("{}".format(url))
+status_code = r.status_code
+
+if status_code >= 400:
+    print("Error code: {}".format(status_code))
+else:
+    print(r.text)
