23cbbed3fdd5687e4c9ff58f6fbfffa03da7138c
Tareq <tareqaldhaifani@gmail.com>
2023-08-26 06:38:43 +0400

    task 0

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A module that fetches and prints data from a url
+"""
+import requests
+
+r = requests.get("https://alu-intranet.hbtn.io/status")
+
+print("Body response:")
+print("    - type: {}".format(type(r.text)))
+print("    - content: {}".format(r.text))
