1f7544ac2816173239eb6a0fd19570dc7b61f1c7
Tareq <tareqaldhaifani@gmail.com>
2023-09-01 16:24:50 +0400

    task 8 - updated comparison to align with PEP8 validation

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     session = factory()
 
     first_state = session.query(State).order_by(State.id).first()
-    if first_state == None:
+    if first_state is None:
         print("Nothing")
     else:
         print("{}: {}".format(first_state.id, first_state.name))
