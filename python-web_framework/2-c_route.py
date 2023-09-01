31517a7d9b610223cc7284f984632ad3322eba52
Tareq <tareqaldhaifani@gmail.com>
2023-09-01 20:19:31 +0400

    task 2

---
+++ python-web_framework/2-c_route.py
@@ -0,0 +1,27 @@
+# /usr/bin/python3
+"""
+a script that starts a Flask web application:
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    msg = "C {}".format(text.replace("_", " "))
+    return msg
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
