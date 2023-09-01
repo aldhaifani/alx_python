7299cffeebe65913c4655825a87407110ba09164
Tareq <tareqaldhaifani@gmail.com>
2023-09-01 20:32:04 +0400

    task 4

---
+++ python-web_framework/4-number_route.py
@@ -0,0 +1,39 @@
+# /usr/bin/python3
+"""
+a script that starts a Flask web application:
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    msg = "C {}".format(text.replace("_", " "))
+    return msg
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def py_text(text="is cool"):
+    msg = "Python {}".format(text.replace("_", " "))
+    return msg
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def int_only(n):
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
