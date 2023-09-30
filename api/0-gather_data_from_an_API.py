ec4c328179fd1610a79ccc9b89c99092d40e19a2
Tareq <tareqaldhaifani@gmail.com>
2023-09-30 15:23:29 +0400

    task 0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+
+user_id = argv[1]
+r_user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(user_id))
+r_todos = requests.get(
+    "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
+)
+
+tasks = r_todos.json()
+completed_tasks_titles = [task["title"] for task in tasks if task["completed"]]
+
+print(
+    "Employee {} is done with tasks({}/{}):".format(
+        r_user.json()["name"], len(completed_tasks_titles), len(tasks)
+    )
+)
+
+for title in completed_tasks_titles:
+    print("\t {}".format(title))
