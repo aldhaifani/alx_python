5b215c097acacc0f0517006de28ef1bcc40975c9
Tareq <tareqaldhaifani@gmail.com>
2023-09-30 15:36:35 +0400

    task 1

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+import csv
+
+user_id = argv[1]
+employee_name = requests.get(
+    "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+).json()["name"]
+tasks = requests.get(
+    "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
+).json()
+
+tasks_data = []
+
+for task in tasks:
+    tasks_data.append(
+        [
+            str(user_id),
+            employee_name,
+            task["completed"],
+            task["title"],
+        ]
+    )
+
+
+with open("{}.csv".format(user_id), "w", encoding="UTF8", newline="") as f:
+    writer = csv.writer(f, delimiter=",")
+    writer.writerows(tasks_data)
